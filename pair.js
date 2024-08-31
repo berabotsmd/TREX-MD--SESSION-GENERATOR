df0f387c6be5aa321be2aeb786d8709e6191e785
berabotsmd <berahiggins@gmail.com>
2024-09-01 02:02:12 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,25 +56,25 @@ router.get('/', async (req, res) => {
                let b64data = Buffer.from(data).toString('base64');
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
-               let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
-*_Made With 🤍_*
+               let GIFTED_MD_TEXT =
+`*_Session Connected By TREX MD_*
+ *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『AMAZING YOU'VE CHOSEN TREX MD』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _youtube.com/@bera_tech
+║❒ *Owner:* _https://wa.me/254743982206_
+║❒ *Repo:* _https://github.com/berabotsmd/TREX-MD_
+║❒ *WaGroup:* _https://chat.whatsapp.com/D3iyJB5hrhF7ov7JxgW92C_
+║❒ *WaChannel:* _https://chat.whatsapp.com/D3iyJB5hrhF7ov7JxgW92C_
+║❒ *Plugins:* _https://github.com/berabotsmd 
 ╚════════════════════════╝
 _____________________________________
-
+	
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
  
